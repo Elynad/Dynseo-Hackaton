5d6222c880e2926caf96eafaf8f0eb8b66a23801
Mathieu Meyer <meyer.mathieu-dev@mailo.com>
2020-10-15 10:07:45 +0200

    Init project

---
+++ app/src/main/java/com/test/dynseo_hackaton/MainActivity.java
@@ -0,0 +1,15 @@
+
+REpackage com.test.dynseo_hackaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
