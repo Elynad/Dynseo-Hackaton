36321baefc4c59ae9b8604236d4b0ce8cf1ffc73
Mathieu Meyer <meyer.mathieu-dev@mailo.com>
2020-10-15 12:21:06 +0200

    Snake working ; need arrows

--- app/src/main/java/com/test/dynseo_hackaton/MainActivity.java
+++ app/src/main/java/com/test/dynseo_hackaton/MainActivity.java
@@ -1,7 +1,11 @@
 package com.test.dynseo_hackaton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,11 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private View gameView;
 
     // D-PAD BUTTONS
-    private Button upButton;
-    private Button downButton;
-    private Button leftButton;
-    private Button rightButton;
+    private AppCompatButton upButton;
+    private AppCompatButton downButton;
+    private AppCompatButton leftButton;
+    private AppCompatButton rightButton;
 
+    SnakeEngine snakeEngine ;
 
     // -- VIEW LIFE CYCLE
     @Override
@@ -35,28 +40,53 @@ public class MainActivity extends AppCompatActivity {
         leftButton = findViewById(R.id.left_button);
         rightButton = findViewById(R.id.right_button);
 
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the SnakeEngine class
+        snakeEngine = new SnakeEngine(this, size);
+
+        // Make snakeEngine the view of the Activity
+        setContentView(snakeEngine);
+
     }
 
 
     // -- METHODS
     public void dPadButtonsOnClick(View v) {
         switch (v.getId()) {
-            case upButton:
-                //heading = Heading.UP;
+            case R.id.up_button:
+                snakeEngine.setHeading(SnakeEngine.Heading.UP);
                 break;
-            case downButton:
-                //doSomething3();
+            case R.id.down_button:
+                snakeEngine.setHeading(SnakeEngine.Heading.DOWN);
                 break;
-            case rightButton:
-                //doSomething4();
+            case R.id.right_button:
+                snakeEngine.setHeading(SnakeEngine.Heading.RIGHT);
                 break;
-            case leftButton:
-                //doSomething5();
+            case R.id.left_button:
+                snakeEngine.setHeading(SnakeEngine.Heading.LEFT);
                 break;
             default:
                 break;
         }
     }
 
+    // Start the thread in snakeEngine
+    @Override
+    protected void onResume() {
+        super.onResume();
+        snakeEngine.resume();
+    }
 
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        snakeEngine.pause();
+    }
 }
